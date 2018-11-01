534f426c56a60be60f373347f41cc46470519887
Svet <39680564+SvitlanaTsupryk-jul18@users.noreply.github.com>
2018-11-01 06:14:25 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+
+gulp.task("default", function() {
+    return gulp.src("./src/script.js")
+        .pipe(babel())
+        .pipe(gulp.dest("./dist"));
+});
